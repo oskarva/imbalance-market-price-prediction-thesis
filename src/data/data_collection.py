ddf0c29df1abcb2c71cf22c3cb1d18a6eac0a48f
Vale <oskar.vaale@gmail.com>
2025-02-06 02:04:43 +0100

    Removing test code in data_collection.py

--- src/data/data_collection.py
+++ src/data/data_collection.py
@@ -1,50 +1,5 @@
 import volue_insight_timeseries 
-import os
 import pandas as pd
-import numpy as np
-
-session = volue_insight_timeseries.Session(config_file=os.environ.get("WAPI_CONFIG"))
-
-basic_curves = [
-"pri de imb up mfrr €/mwh cet min15 a",
-"pri de imb down mfrr €/mwh cet min15 a",
-"vol de imb sys mw cet min15 a",
-"vol de imb up mw cet min15 a",
-"vol de imb down mw cet min15 a",
-"con de intraday mwh/h cet min15 a",
-"con de heating % cet min15 s",
-"con de cooling % cet min15 s",
-"pro de tot mwh/h cet min15 a",
-"vol de cap imb up mfrr mw cet h a",
-"vol de cap imb down mfrr mw cet h a",
-"vol de cap imb up afrr mw cet h a",
-"vol de cap imb down afrr mw cet h a",
-"pri de spot €/mwh cet h a",
-"rdl de mwh/h cet min15 a",
-"pri de imb stlmt €/mwh cet min15 a",
-] 
-
-exchange_curves = [
-    "exc de>se4 com mw cet h a",
-    "exc de>dk1 com mw cet h a",
-    "exc de>dk2 com mw cet h a",
-    "exc de>nl com mw cet h a",
-    "exc de>fr com mw cet h a",
-    "exc de>ch com mw cet h a",
-    "exc de>at com mw cet h a",
-    "exc de>cz com mw cet h a",
-    "exc de>pl com mw cet h a",
-    "exc de>se com mw cet h a",
-    "exc de>no2 com mw cet h a",
-    "exc de>be com mw cet h a",
-    "exc de>no com mw cet h a",
-    "exc de>dk com mw cet h a"
-]
-
-combined = basic_curves + exchange_curves
-start_date = pd.Timestamp("2021-01-01")
-end_date = pd.Timestamp.today()
-
 
 def get_data(X_curve_names: list, y_curve_names: list, 
              session: volue_insight_timeseries.Session,  
@@ -118,16 +73,3 @@ def _get_data(curve_names: list, target_columns: list,
     cleaned_df = combined_df.dropna()
 
     return cleaned_df
-
-
-if __name__ == "__main__":
-    # Example usage: here we choose some curves for X and target curves for y
-    X, y = get_data(combined, 
-                    ["pri de imb up afrr €/mwh cet min15 a", "pri de imb down afrr €/mwh cet min15 a"],
-                    session, 
-                    start_date, 
-                    end_date)
-    
-    # Now X and y are numpy arrays ready for ML algorithms.
-    print("X shape:", X.shape)
-    print("y shape:", y.shape)
